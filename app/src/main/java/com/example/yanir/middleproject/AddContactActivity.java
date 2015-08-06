19c53d219106207ff4d0ffe2afdc1dd4530bb6aa
yanirco <yanir.cohen@gmail.com>
2015-08-06 20:39:23 +0300

    Merge branch 'master' of github.com:yanirco/YanirIsLearning
    Conflicts:
    	app/src/main/java/com/example/yanir/middleproject/MainActivity.java

--- app/src/main/java/com/example/yanir/middleproject/AddContactActivity.java
+++ app/src/main/java/com/example/yanir/middleproject/AddContactActivity.java
@@ -3,10 +3,11 @@ package com.example.yanir.middleproject;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.view.View.OnClickListener;
-
+import android.widget.EditText;
 
 
 public class AddContactActivity extends Activity {
@@ -16,45 +17,44 @@ public class AddContactActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_contact);
         whenClickCancelSaveButton();
+        whenClickAddPictureButton();
+        EditText contactname = (EditText) findViewById(R.id.contactName);
+        EditText contactaddress = (EditText) findViewById(R.id.contactAdress);
+        EditText contactemail = (EditText) findViewById(R.id.contactEmail);
+        EditText contactphone = (EditText) findViewById(R.id.contactPhone);
+        EditText contactbirthdate = (EditText) findViewById(R.id.contctBirthDate);
+        EditText contacttimetocall = (EditText) findViewById(R.id.contactTimeToCal);
+        EditText contactdatetocall = (EditText) findViewById(R.id.contactTDateToCall);
+        EditText contactName = (EditText) findViewById(R.id.contactName);
+
     }
-    public void whenClickCancelSaveButton() {
-        Button cancelButton = (Button) findViewById(R.id.cancelsavecontactbutton);
-        cancelButton.setOnClickListener(new OnClickListener() {
+
+    public void whenClickAddPictureButton() {
+        Button addContactPictureBtn;
+        addContactPictureBtn = (Button) findViewById(R.id.contactAddPictureBtn);
+        addContactPictureBtn.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent myIntent = new Intent(MainActivity.this, MainActivity.class);
-                AddContactActivity.this.startActivity(myIntent);
+                final int REQUEST_IMAGE_CAPTURE = 1;
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
             }
         });
 
+
     }
 
+    public void whenClickCancelSaveButton() {
+        Button cancelButton = (Button) findViewById(R.id.cancelsavecontactbutton);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+
+        });
 
 
-
-
-
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_add_contact, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
