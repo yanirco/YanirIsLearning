a6a008bd72d0b8b15ed8730732bc0acd0e550473
yanirco <yanir.cohen@gmail.com>
2015-08-06 17:22:29 +0300

    Merge branch 'master' of github.com:yanirco/YanirIsLearning
    Conflicts:
    	app/src/main/java/com/example/yanir/middleproject/MainActivity.java

--- app/src/main/java/com/example/yanir/middleproject/AddContactActivity.java
+++ app/src/main/java/com/example/yanir/middleproject/AddContactActivity.java
@@ -1,22 +1,33 @@
 package com.example.yanir.middleproject;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.view.View.OnClickListener;
 
 
-public class AddContactActivity extends ActionBarActivity {
+
+public class AddContactActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_contact);
+        whenClickCancelSaveButton();
     }
+    public void whenClickCancelSaveButton() {
+        Button cancelButton = (Button) findViewById(R.id.cancelsavecontactbutton);
+        cancelButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, MainActivity.class);
+                AddContactActivity.this.startActivity(myIntent);
+            }
+        });
 
-    Button saveButton  = (Button) findViewById(save)
-
+    }
 
 
 
